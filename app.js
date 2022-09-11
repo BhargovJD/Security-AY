1c3c6188590a4b148528fcf87a2ec3e2802108a0
Bhargov Jyoti Das <bhargovjyotidas1996@gmail.com>
2022-09-11 18:46:12 +0530

    Level 3 - Hash Password

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-var encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const port = 3000;
  
 const app = express();
 
-// console.log(process.env.SECRET);
+// console.log(md5("qwerty"));
  
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -29,7 +29,6 @@ const userSchema = new mongoose.Schema({
 
 
   
-  userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields:['password'] });
 
 
 
@@ -54,7 +53,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
     const newUser  = new User({
         email:req.body.username,
-        password:req.body.password,
+        password:md5(req.body.password),
     })
 
     newUser.save(function(err){
@@ -72,7 +71,7 @@ app.post("/register", function(req,res){
 app.post("/login", function(req,res){
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username}, function(err,foundUser){
         if(err){
